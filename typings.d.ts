4b97f019f79b614678421dc7a1539dccaba94949
Marvin Hagemeister <marvin.hagemeister@sevenval.com>
2016-10-28 17:06:02 +0200

    Fix wrong typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,5 @@
 declare module 'nicer-fs' {
   export function find(globber: string): Promise<string[]>;
   export function readFile(path: string): Promise<string>;
-  export function writeFile(path: string, data: string | Buffer): void;
+  export function writeFile(path: string, data: string | Buffer): Promise<void>;
 }
