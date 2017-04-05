eb4be48018337c026f3060ba1190862a528f07cb
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-04-05 07:41:50 +0200

    Udpate typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,7 +1,8 @@
-declare module 'nicer-fs' {
+declare module "nicer-fs" {
   import glob = require("glob");
 
   export function find(globber: string, options?: glob.IOptions): Promise<string[]>;
   export function readFile(path: string): Promise<string>;
   export function writeFile(path: string, data: string | Buffer): Promise<void>;
+  export function readDir(folder: string): Promise<string[]>;
 }
