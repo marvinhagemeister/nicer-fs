97fcce50487f7c86992645101e2c1828d87f2cad
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-04-15 18:06:47 +0200

    Fix readFile typings

--- typings.d.ts
+++ typings.d.ts
@@ -2,7 +2,8 @@ declare module "nicer-fs" {
   import glob = require("glob");
 
   export function find(globber: string, options?: glob.IOptions): Promise<{}>;
-  export function readFile(filepath: string, encoding?: string): Promise<{}>;
+  export function readFile(filepath: string): Promise<Buffer>;
+  export function readFile(filepath: string, encoding: string): Promise<string>;
   export function writeFile(filepath: string, data: string | Buffer, options?: any): Promise<{}>;
   export function readDir(folder: string): Promise<string[]>;
   export function mkdir(dir: string, flags?: any): Promise<string>;
